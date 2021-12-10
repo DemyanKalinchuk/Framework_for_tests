b70cb15d8a243e67177224008785ebd21908ce86
Demian Kalinchuk <demonation20@gmail.com>
2021-12-10 19:51:51 +0200

    Initial commit new frame fo site

---
+++ src/main/java/pages/home/HomePage.java
@@ -0,0 +1,21 @@
+package pages.home;
+
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+private final By clickreg = (By.xpath("input[@class='search-form__input ng-pristine ng-valid ng-touched']"));
+
+public HomePage clickSearch(){
+driver.findElement(clickreg).sendKeys("Видеокарти", Keys.ENTER);
+    return this;
+}
+
+}
---
+++ src/test/java/tests/searchapartment/SearchSomeProductTest.java
@@ -0,0 +1,19 @@
+package tests.searchapartment;
+
+import org.testng.annotations.Test;
+import tests.base.BaseTest;
+
+import static Constants.Constant.Urls.REALT_HOME_PAGE;
+
+public class SearchSomeProductTest extends BaseTest {
+    @Test
+    public void checkRedirectToListing (){
+
+        basePage.show(REALT_HOME_PAGE);
+        homePage
+                .clickSearch();
+
+        realtListingPage.checkCountCards();
+    }
+
+}
